d0da8730cde54ac2f7770f693297e7e0ddf44e18
Mateusz Błajda <mateuszblajda99@gmail.com>
2024-07-02 13:39:52 +0200

    script changed for bilstm_att model

--- code/full_sequences_benchmark/full_seq_predict.py
+++ code/full_sequences_benchmark/full_seq_predict.py
@@ -17,17 +17,17 @@ print(f"Device: {DEVICE}")
 # dataset_path = './data/benchmark/lt2_windows__cvs_gt2.csv'
 dataset_path = './data/benchmark/full_test.csv'
 
-model_c_json_path = './code/run_jsons/c_bilstm.json'
-model_n_json_path = './code/run_jsons/n_bilstm.json'
+# model_c_json_path = './code/run_jsons/c_bilstm.json'
+# model_n_json_path = './code/run_jsons/n_bilstm.json'
 
-model_c_params = './params/models/c_BiLSTM.pt'
-model_n_params = './params/models/n_BiLSTM.pt'
+# model_c_params = './params/models/c_BiLSTM.pt'
+# model_n_params = './params/models/n_BiLSTM.pt'
 
-# model_c_json_path = './code/run_jsons/c_bilstm_att.json'
-# model_n_json_path = './code/run_jsons/n_bilstm_att.json'
+model_c_json_path = './code/run_jsons/c_bilstm_att.json'
+model_n_json_path = './code/run_jsons/n_bilstm_att.json'
 
-# model_c_params = './params/models/c_BiLSTM_att.pt'
-# model_n_params = './params/models/n_BiLSTM_att.pt'
+model_c_params = './params/models/c_BiLSTM_att.pt'
+model_n_params = './params/models/n_BiLSTM_att.pt'
 
 # model_c_json_path = './code/run_jsons/c_esm2.json'
 # model_n_json_path = './code/run_jsons/n_esm2.json'
@@ -57,13 +57,13 @@ vocab = torch.load("./params/vocab.pt").to(DEVICE)
 model_c_conf = {
     "vocab_size": len(vocab),
     "embedding_dim": model_c_json['embedding_dim'],
-    # "rnn_size": model_c_json['rnn_size1'],
-    "rnn_size1": model_c_json['rnn_size1'],
-    "rnn_size2": model_c_json['rnn_size2'],
+    "rnn_size": model_c_json['rnn_size1'],
+    # "rnn_size1": model_c_json['rnn_size1'],
+    # "rnn_size2": model_c_json['rnn_size2'],
     "hidden_size": model_c_json['linear_size1'],
     "dropout": model_c_json['dropout'],
     "out_neurons": model_c_json['out_neurons'],
-    # "num_heads": model_c_json['num_heads1'],
+    "num_heads": model_c_json['num_heads1'],
     "seq_len": model_c_json['seq_len'],
     "batch_norm": model_c_json['batch_norm'],
 }
@@ -71,13 +71,13 @@ model_c_conf = {
 model_n_conf = {
     "vocab_size": len(vocab),
     "embedding_dim": model_n_json['embedding_dim'],
-    # "rnn_size": model_n_json['rnn_size1'],
-    "rnn_size1": model_n_json['rnn_size1'],
-    "rnn_size2": model_n_json['rnn_size2'],
+    "rnn_size": model_n_json['rnn_size1'], # bilstm_att
+    # "rnn_size1": model_n_json['rnn_size1'], # bilstm
+    # "rnn_size2": model_n_json['rnn_size2'], # bilstm
     "hidden_size": model_n_json['linear_size1'],
     "dropout": model_n_json['dropout'],
     "out_neurons": model_n_json['out_neurons'],
-    # "num_heads": model_n_json['num_heads1'],
+    "num_heads": model_n_json['num_heads1'], # bilstm_att
     "seq_len": model_n_json['seq_len'],
     "batch_norm": model_n_json['batch_norm'],
 }
@@ -96,11 +96,11 @@ model_n_conf = {
 
 print("Loading models")
 
-model_c = BiLSTM(**model_c_conf).to(DEVICE)
-model_n = BiLSTM(**model_n_conf).to(DEVICE)
+# model_c = BiLSTM(**model_c_conf).to(DEVICE)
+# model_n = BiLSTM(**model_n_conf).to(DEVICE)
 
-# model_c = BiLSTMAttention(**model_c_conf).to(DEVICE)
-# model_n = BiLSTMAttention(**model_n_conf).to(DEVICE)
+model_c = BiLSTMAttention(**model_c_conf).to(DEVICE)
+model_n = BiLSTMAttention(**model_n_conf).to(DEVICE)
 
 # model_c = ESM2(**model_c_conf).to(DEVICE)
 # model_n = ESM2(**model_n_conf).to(DEVICE)
@@ -148,5 +148,5 @@ for data in tqdm(dataset):
     c_preds_list.append(c_preds.detach().cpu().numpy())
 
 print("Saving predictions")
-np.save(f'{results_path}/n_bilstm.npy', np.array(n_preds_list, dtype=object), allow_pickle=True)
-np.save(f'{results_path}/c_bilstm.npy', np.array(c_preds_list, dtype=object), allow_pickle=True)
+np.save(f'{results_path}/n_bilstm_att.npy', np.array(n_preds_list, dtype=object), allow_pickle=True)
+np.save(f'{results_path}/c_bilstm_att.npy', np.array(c_preds_list, dtype=object), allow_pickle=True)
