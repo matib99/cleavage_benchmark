0246072b49f41fba951a0736b2e0c1e8529bff94
Mateusz Błajda <mateuszblajda99@gmail.com>
2024-08-19 18:51:36 +0200

    calculate metrics update

--- code/full_sequences_benchmark/calculate_metrics.py
+++ code/full_sequences_benchmark/calculate_metrics.py
@@ -4,8 +4,18 @@ import pandas as pd
 from ast import literal_eval
 import os
 import sys
+import matplotlib.pyplot as plt
+from sklearn import metrics
 
-run_name="bilstm_their_data"
+
+model_name = "bilstm"
+# model_name = "bilstm_att"
+# model_name = "esm2"
+
+training_set="their"
+# training_set="my"
+
+run_name=f"{model_name}_{training_set}_data"
 
 dir_path=f"{sys.argv[1]}/{run_name}"
 os.makedirs(dir_path, exist_ok=True)
@@ -13,37 +23,23 @@ os.makedirs(dir_path, exist_ok=True)
 raport=""
 raport += f"{run_name}\n"
 
-# %%
 dataset_path = './data/benchmark/full_test.csv'
 # dataset_path = './data/benchmark/lt2_windows__cvs_gt2.csv'
 # dataset_path = '../../data/benchmark/multiple_windows.csv'
 
 
-n_preds_path = './data/benchmark/preds_my/n_bilstm.npy'
-c_preds_path = './data/benchmark/preds_my/c_bilstm.npy'
-# n_preds_path = '../../data/benchmark/preds/n_bilstm_att.npy'
-# c_preds_path = '../../data/benchmark/preds/c_bilstm_att.npy'
-# n_preds_path = '../../data/benchmark/preds/n_bilstm.npy'
-# c_preds_path = '../../data/benchmark/preds/c_bilstm.npy'
-# n_preds_path = '../../data/benchmark/preds/n_bilstm_mw.npy'
-# c_preds_path = '../../data/benchmark/preds/c_bilstm_mw.npy'
+n_preds_path = f'./data/benchmark/preds_{training_set}/n_{model_name}.npy'
+c_preds_path = f'./data/benchmark/preds_{training_set}/c_{model_name}.npy'
 
-# %%
 data_df = pd.read_csv(dataset_path)
 data_df.head()
 
-# %%
 c_preds = np.load(c_preds_path, allow_pickle=True)
 n_preds = np.load(n_preds_path, allow_pickle=True)
 
-# %%
 cleavages = data_df['cleavages'].apply(literal_eval).values
 seq_lens = data_df['protein'].apply(len).values
 
-# %%
-
-
-# %%
 n_targets = [np.zeros(seq_len + 1) for seq_len in seq_lens]
 c_targets = [np.zeros(seq_len + 1) for seq_len in seq_lens]
 
@@ -52,30 +48,22 @@ for i, cleavage in enumerate(cleavages):
         n_targets[i][n - 1] = 1
         c_targets[i][c] = 1
 
-# %%
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-# %%
 n_preds = [sigmoid(pred) for pred in n_preds]
 c_preds = [sigmoid(pred) for pred in c_preds]
 
-
-
-# %%
 c_preds_concat = np.concatenate(c_preds)
 n_preds_concat = np.concatenate(n_preds)
 
 c_targets_concat = np.concatenate(c_targets)
 n_targets_concat = np.concatenate(n_targets)
 
-# %%
 print("calculate roc auc")
-from sklearn import metrics
 
-# %%
-fpr_c, tpr_c, thresholds_c = metrics.roc_curve(c_targets_concat, c_preds_concat)
-fpr_n, tpr_n, thresholds_n = metrics.roc_curve(n_targets_concat, n_preds_concat)
+fpr_c, tpr_c, _ = metrics.roc_curve(c_targets_concat, c_preds_concat)
+fpr_n, tpr_n, _ = metrics.roc_curve(n_targets_concat, n_preds_concat)
 
 roc_auc_c = metrics.auc(fpr_c, tpr_c)
 roc_auc_n = metrics.auc(fpr_n, tpr_n)
@@ -87,10 +75,6 @@ raport+=f"N terminus: {roc_auc_n}\n"
 raport+=f"C terminus: {roc_auc_c}\n"
 raport+="\n"
 
-# %%
-import matplotlib.pyplot as plt
-
-# %%
 # plot ROC curves for both models (on separate plots):
 
 fig, ax = plt.subplots(1, 2, figsize=(12, 6))
@@ -109,29 +93,57 @@ ax[1].set_title('N - Terminus ROC Curve')
 ax[1].legend()
 
 plt.savefig(f"{dir_path}/roc_auc.png")
-# %%
+
+# PR Plot and AUC
+
+precisions_c, recalls_c, _ = metrics.precision_recall_curve(c_targets_concat, c_preds_concat)
+precisions_n, recalls_n, _ = metrics.precision_recall_curve(n_targets_concat, n_preds_concat)
+pr_auc_c = metrics.auc(recalls_c, precisions_c)
+pr_auc_n = metrics.auc(recalls_n, precisions_n)
+
+print(f"C - Terminus PR AUC: {pr_auc_c}")
+print(f"N - Terminus PR AUC: {pr_auc_n}")
+raport+="PR AUC:\n"
+raport+=f"N terminus: {pr_auc_n}\n"
+raport+=f"C terminus: {pr_auc_c}\n"
+raport+="\n"
+
+# Plot PR curves for both models (on separate plots):
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+ax[0].plot(recalls_c, precisions_c, label=f"C - Terminus PR AUC: {pr_auc_c}")
+ax[0].set_xlabel('Recall')
+ax[0].set_ylabel('Precision')
+ax[0].set_title('C - Terminus PR Curve')
+ax[0].legend()
+
+ax[1].plot(recalls_n, precisions_n, label=f"N - Terminus PR AUC: {pr_auc_n}")
+ax[1].set_xlabel('Recall')
+ax[1].set_ylabel('Precision')
+ax[1].set_title('N - Terminus PR Curve')
+ax[1].legend()
+
+plt.savefig(f"{dir_path}/pr_auc.png")
+
 trh = 0.5
 
 print("calculate precission/recall")
-# %%
 raport+="\n"
 raport+="PRECISSION:\n"
 raport+=f"C Terminus: {metrics.precision_score(c_targets_concat, c_preds_concat > trh)}\n"
 raport+=f"N Terminus: {metrics.precision_score(n_targets_concat, n_preds_concat > trh)}\n"
 
-# %%
 raport+="\n"
 raport+="RECALL:\n"
 raport+=f"C Terminus: {metrics.recall_score(c_targets_concat, c_preds_concat > trh)}\n"
 raport+=f"N Terminus: {metrics.recall_score(n_targets_concat, n_preds_concat > trh)}\n"
 
-# %%
 raport+="\n"
 raport+="F1 SCORE:\n"
 raport+=f"C Terminus: {metrics.f1_score(c_targets_concat, c_preds_concat > trh)}\n"
 raport+=f"N Terminus: {metrics.f1_score(n_targets_concat, n_preds_concat > trh)}\n"
-print("posotive rank")
-# %%
+
+print("positive rank")
 # N terminus positives rank
 n_pos_quantiles = []
 
@@ -197,7 +209,6 @@ ax[1].set_ylabel('Frequency')
 
 plt.savefig(f"{dir_path}/positive_ranks.png")
 
-# %%
 # Heuristic epitope retrieval
 
 def get_epitopes(n_pred, c_pred, min_epitope_len=4, max_epitope_len=16, threshold=0.1):
